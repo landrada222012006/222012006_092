0790e3a505c7aca2529506bf7ecc33e379c91028
landrada222012006 <149480844+landrada222012006@users.noreply.github.com>
2023-11-03 12:26:33 +0200

    Add files via upload

---
+++ breakstatement.java
@@ -0,0 +1,11 @@
+public class breakstatement {
+    public static void main(String[] args) {
+        for(int i=7;i>=1;i--){
+            if(i==3){
+
+                break;
+            }
+            System.out.println(i);
+    }} }
+    
+
---
+++ nestedforloop.java
@@ -0,0 +1,9 @@
+public class nestedforloop {
+    public static void main(String[] args) {
+        for(int j=4;j>=3;j--){
+            for(int i=4;i>=1;i--){
+                System.out.println(j+" "+i);
+            }
+        }
+    }
+}
